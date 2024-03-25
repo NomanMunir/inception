82e1085d677dd4494af95a1d8c1580286d11291f
Nauman Munir <mrnomanmunir@gmail.com>
2024-03-25 17:32:18 +0400

    fix mariadb now it's working.

--- requirements/mariadb/tools/set_db.sh
+++ requirements/mariadb/tools/set_db.sh
@@ -1,14 +1,31 @@
 cat .setup 2> /dev/null
 if [ $? -ne 0 ]; then
-	usr/bin/mysqld_safe --datadir=/var/lib/mysql &
+    # Start MySQL/MariaDB service
+    /usr/bin/mysqld_safe --datadir=/var/lib/mysql &
 
+    # Wait for MySQL/MariaDB service to become available
+    while ! mysqladmin ping -h "$MYSQL_HOSTNAME" --silent; do
+        sleep 1
+    done
 
-	while ! mysqladmin ping -h "$MYSQL_HOSTNAME" --silent; do
-    	sleep 1
-	done
+    # Initialize database using init_db.sql
+    eval "echo \"$(cat /tmp/init_db.sql)\"" | mariadb
 
-	eval "echo \"$(cat /tmp/init_db.sql)\"" | mariadb
-	touch .setup
+    # Create .setup file to indicate setup completion
+
+    touch .setup
 fi
 
-# usr/bin/mysqld_safe --datadir=/var/lib/mysql
+    # If .setup file exists, only start MySQL/MariaDB service
+/usr/bin/mysqld_safe --datadir=/var/lib/mysql
+
+while true; do
+    sleep 1
+done
+# Once inside the container, you can execute MySQL commands like:
+# mysql -u root -p (then enter root password)
+# SHOW DATABASES;
+# use 'wordpress';
+# SHOW TABLES;
+# SELECT wp_users.display_name FROM wp_users;
+# SELECT * FROM wp_users;
