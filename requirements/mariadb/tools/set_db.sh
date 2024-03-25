f0aab0882de1077ae241c256325bb9c2687bd9bc
Nauman Munir <mrnomanmunir@gmail.com>
2024-03-25 13:09:16 +0400

    nginx and wordpress is working now need to connect mariadb with wordpress

---
+++ requirements/mariadb/tools/set_db.sh
@@ -0,0 +1,14 @@
+cat .setup 2> /dev/null
+if [ $? -ne 0 ]; then
+	usr/bin/mysqld_safe --datadir=/var/lib/mysql &
+
+
+	while ! mysqladmin ping -h "$MYSQL_HOSTNAME" --silent; do
+    	sleep 1
+	done
+
+	eval "echo \"$(cat /tmp/init_db.sql)\"" | mariadb
+	touch .setup
+fi
+
+# usr/bin/mysqld_safe --datadir=/var/lib/mysql
---
+++ requirements/wordpress/start-php-fpm.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# start-php-fpm.sh
+
+# Make any last-minute config changes
+# For example, adjusting www.conf based on environment variables (if necessary)
+
+# Start PHP-FPM in the foreground
+php-fpm -F
